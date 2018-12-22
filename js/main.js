4858aa87a8d1dd6b45c2d8b0bf773981039c8232
nocoldiz <nocoldin@gmail.com>
2018-12-22 13:11:27 +0100

    Added data loading via json

--- js/main.js
+++ js/main.js
@@ -1,6 +1,69 @@
 
 
-
+var json = {
+  "partnersBasic": "100",
+  "partnersPremium": "20",
+  "partnersGrowth": "30",
+  "totalReviews": "2000",
+  "tripadvisorTotalReviews": "10",
+  "googleplacesTotalReviews": "30",
+  "modelsRanking": {
+    "0": {
+      "photo": "photo.jpg",
+      "name": "Sunrina Porfirova",
+      "reviews": "120",
+      "stories": "300",
+      "earned": "100",
+      "spent": "300"
+    },
+    "1": {
+      "photo": "photo.jpg",
+      "name": "Elizabeth Bohem",
+      "reviews": "120",
+      "stories": "300",
+      "earned": "100",
+      "spent": "300"
+    }
+  },
+  "restaurantsRanking": {
+    "0": {
+      "photo": "photo.jpg",
+      "name": "La perla d'oro",
+      "reviews": "120",
+      "visitors": "300",
+      "credits": "300"
+    }
+  },
+  "dailyContent": {
+    "0": {
+      "photo": "photo.jpg",
+      "name": "La perla d'oro",
+      "instaStories": "120",
+      "instaPosts": "300",
+      "tripadvisorReviews": "100",
+      "googleplacesReviews": "300",
+      "facebookReviews": "300",
+      "balance": "1020"
+    }
+  },
+  "credits_given" :"3000",
+  "credits_back": "50",
+  "insta_stories": "2500",
+  "maps_reviews": "300",
+  "insta_posts": "402",
+  "trip_reviews": "50",
+  "wallet_ranking": {
+    "0": {
+      "photo": "photo.jpg",
+      "name": "La perla d'oro",
+      "months": "120",
+      "credits": "300",
+      "plan": "100",
+      "price": "300",
+    }
+  }
+}
+// require("./data.json")
 function changeSection(){
 	$( "#sectionProfile").hide();
 	$( "#sectionOverview").hide();
@@ -9,7 +72,10 @@ function changeSection(){
 	$( "#sectionMembers").hide();
 
 }
+
+
 changeSection();
+loadData(json);
 $( "#sectionOverview").show();
 
 $( "#load_profile" ).click(function() {
@@ -47,3 +113,22 @@ $("#daily_tab").click(function() {
 	$( ".dailyContent").show();	
 	$( ".fullrecordContent").hide();
 });
+
+
+$.getJSON("./js/data.json",function(json){
+	console.log(json)
+});
+
+
+
+function loadData(data){
+	//$( ".partnersBasic").text(data['partnersBasic']);
+	$.each(data,function(i, item){
+		console.log(i, item);
+		$('#'+i).text(item);
+
+	});
+
+
+
+}
